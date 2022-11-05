b57cb034d8f62e000ad116789dc7ccbe5cbfc38b
Lucas <lucas.friedmann35@live.ca>
2022-11-05 00:15:12 -0400

    add cars model, index cars

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Car:
+    def __init__(self, make, model, year, colour):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.colour = colour
+
+cars = [
+    Car('Mercedes-Benz', 'E-Class', '2022', 'red'),
+    Car('Infiniti', 'Q40', '2015', 'black'),
+    Car('Genesis', 'G80', '2019', 'white'),
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse("YOOOO")
 
 def about(request):
     return render(request, 'about.html')
+
+def cars_index(request):
+    return render(request, 'cars/index.html', {'cars': cars })
