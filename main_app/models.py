180eb623c046a69cbb89a2d49590ea8f83409f94
Lucas <lucas.friedmann35@live.ca>
2022-11-07 15:43:39 -0500

    compile cars model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField(max_length=4)
+    color = models.CharField(max_length=100)
+    kms = models.IntegerField(max_length=15)
+    trans = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.model
