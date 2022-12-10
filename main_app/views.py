57c183c9106051e68b72f4e25f36319080b961b5
Lucas <lucas.friedmann35@live.ca>
2022-12-10 01:51:22 -0500

    complete part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cars_index(request):
+    cars = Car.objects.all()
     return render(request, 'cars/index.html', {'cars': cars })
+
+def cars_detail(request, car_id):
+  car = Car.objects.get(id=car_id)
+  return render(request, 'cars/detail.html', { 'car': car })
